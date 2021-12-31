34f04eb26ac5e945d3cb0a1552f13d8fa699d415
cinob <cinob@foxmail.com>
2021-12-31 22:41:19 +0800

    ✨ ssr

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-const _ = require('koa-route')
 const express = require('express')
 
-async function createServer (
-  root = process.cwd()
-) {
+async function createServer () {
   const resolve = (p: string) => path.resolve(__dirname, p)
 
   const indexProd = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
@@ -13,7 +10,6 @@ async function createServer (
 
   const app = new express()
 
-  let vite
   app.use(require('compression')())
   app.use(
     require('serve-static')(resolve('dist/client'), {
@@ -21,11 +17,18 @@ async function createServer (
     })
   )
 
-  app.use('*', async (req, res) => {
-    const url = req.originalUrl
+  app.use('*', async (req: any, res: any) => {
+    // const url = req.originalUrl
     let template, render
     template = indexProd
-        render = require('./dist/server/entry-server.js').render
+    render = require('./dist/server/entry-server.js').render
+
+    const [appHtml, preloadLinks] = await render( manifest)
+    const html = template
+      .replace(`<!--preload-links-->`, preloadLinks)
+      .replace(`<!--app-html-->`, appHtml)
+
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
   })
   return { app }
 }
